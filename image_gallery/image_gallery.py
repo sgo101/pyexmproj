ebc49ed14c27162ee7386d8be9d7676a7e5b515a
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-23 03:13:06 +0330

    finish ui

--- image_gallery/image_gallery.py
+++ image_gallery/image_gallery.py
@@ -3,6 +3,9 @@ import os
 import tkinter as tk
 import tkinter.ttk as ttk
 
+from PIL import ImageTk, Image
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGES_PATH = os.path.join(BASE_DIR, 'images')
 
@@ -10,24 +13,41 @@ IMAGES_PATH = os.path.join(BASE_DIR, 'images')
 
 root = tk.Tk()
 root.title("Image Gallery")
+root.geometry("600x500")
 
 mainframe = ttk.Frame(root, padding=10)
+info_frame = ttk.Frame(mainframe)
 image_frame = ttk.Frame(mainframe, relief=tk.SOLID)
-tool_frame = ttk.Frame(mainframe, relief=tk.GROOVE)
+tool_frame = ttk.Frame(mainframe)
 mainframe.pack(expand=True, fill=tk.BOTH)
-image_frame.grid(sticky=(tk.E, tk.W, tk.S, tk.N))
-tool_frame.grid(row=1, column=0, sticky=(tk.E, tk.W))
+info_frame.grid(stick=(tk.E, tk.W))
+image_frame.grid(row=1, column=0, sticky=(tk.E, tk.W, tk.S, tk.N))
+tool_frame.grid(row=2, column=0, sticky=(tk.E, tk.W))
 
-# img1 = tk.PhotoImage(file='python_logo.gif')
-# img_label1 = ttk.Label(image_frame, image=img1)
-# img_label1.pack()
+img_filename_label = ttk.Label(info_frame, text="Image filename")
+img_filename_label.pack(side=tk.LEFT, padx=3)
 
-next_button = ttk.Button(tool_frame, text="Next")
+
+# Image
+size = (300, 300)
+img = Image.open(os.path.join(IMAGES_PATH, 'img1.jpg'))
+img.thumbnail(size)
+img_tk = ImageTk.PhotoImage(img)
+label = ttk.Label(image_frame, image=img_tk)
+label.pack(expand=True)
+
+
 previous_button = ttk.Button(tool_frame, text="Previous")
-next_button.grid()
-previous_button.grid()
+next_button = ttk.Button(tool_frame, text="Next")
+previous_button.grid(row=0, column=0, sticky=tk.W)
+next_button.grid(row=0, column=1, sticky=tk.E)
+tool_frame.columnconfigure(0, weight=1)
+tool_frame.columnconfigure(1, weight=1)
 
 for child in mainframe.winfo_children():
 	child.grid_configure(padx=10, pady=10)
+
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(1, weight=1)
 
 root.mainloop()
