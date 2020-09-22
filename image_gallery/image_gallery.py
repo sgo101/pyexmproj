112159cea82cb61973d17e1e67901552b60234ff
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-23 01:49:18 +0330

    config images path

--- image_gallery/image_gallery.py
+++ image_gallery/image_gallery.py
@@ -1,5 +1,10 @@
+import os
+
 import tkinter as tk
 import tkinter.ttk as ttk
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGES_PATH = os.path.join(BASE_DIR, 'images')
 
 
 
@@ -13,12 +18,12 @@ mainframe.pack(expand=True, fill=tk.BOTH)
 image_frame.grid(sticky=(tk.E, tk.W, tk.S, tk.N))
 tool_frame.grid(row=1, column=0, sticky=(tk.E, tk.W))
 
-img1 = tk.PhotoImage(file='python_logo.gif')
-img_label1 = ttk.Label(image_frame, image=img1)
-img_label1.pack()
+# img1 = tk.PhotoImage(file='python_logo.gif')
+# img_label1 = ttk.Label(image_frame, image=img1)
+# img_label1.pack()
 
-next_button = ttk.Button(tool_frame)
-previous_button = ttk.Button(tool_frame)
+next_button = ttk.Button(tool_frame, text="Next")
+previous_button = ttk.Button(tool_frame, text="Previous")
 next_button.grid()
 previous_button.grid()
 
