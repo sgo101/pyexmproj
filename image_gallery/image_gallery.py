38a69d3d8b0c0298a901a1d224d4ba275cd06dd4
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-23 04:17:51 +0330

    read all images and display first one when app init

--- image_gallery/image_gallery.py
+++ image_gallery/image_gallery.py
@@ -1,13 +1,38 @@
 import os
+import glob
 
 import tkinter as tk
 import tkinter.ttk as ttk
 
 from PIL import ImageTk, Image
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGES_PATH = os.path.join(BASE_DIR, 'images')
+size = (300, 300)
+
+
+def images():
+	jpgs = glob.glob(os.path.join(IMAGES_PATH, "*.jpg"))
+	pngs = glob.glob(os.path.join(IMAGES_PATH, "*.png"))
+	return jpgs + pngs
+
+
+
+def on_next():
+	second_img = images()[1]
+	with Image.open(second_img) as img_file:
+		img_file.thumbnail(size)
+		label.img = ImageTk.PhotoImage(img_file)
+		label.config(image=label.img)
+
+
+
+def on_previous():
+	pass
+
+
+
+
 
 
 
@@ -28,17 +53,16 @@ img_filename_label = ttk.Label(info_frame, text="Image filename")
 img_filename_label.pack(side=tk.LEFT, padx=3)
 
 
-# Image
-size = (300, 300)
-img = Image.open(os.path.join(IMAGES_PATH, 'img1.jpg'))
-img.thumbnail(size)
-img_tk = ImageTk.PhotoImage(img)
-label = ttk.Label(image_frame, image=img_tk)
+# Set first image to label
+img_file = Image.open(images()[0])
+img_file.thumbnail(size)
+img = ImageTk.PhotoImage(img_file)
+label = ttk.Label(image_frame, image=img)
 label.pack(expand=True)
 
 
-previous_button = ttk.Button(tool_frame, text="Previous")
-next_button = ttk.Button(tool_frame, text="Next")
+previous_button = ttk.Button(tool_frame, text="Previous", command=on_previous)
+next_button = ttk.Button(tool_frame, text="Next", command=on_next)
 previous_button.grid(row=0, column=0, sticky=tk.W)
 next_button.grid(row=0, column=1, sticky=tk.E)
 tool_frame.columnconfigure(0, weight=1)
