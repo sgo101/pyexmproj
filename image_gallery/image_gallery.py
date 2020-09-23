a567f750ab892872dde9066f5fa01f1825fe96a1
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-23 05:46:03 +0330

    Update image thumbnail based on winodow size

--- image_gallery/image_gallery.py
+++ image_gallery/image_gallery.py
@@ -13,7 +13,7 @@ IMAGES_PATH = os.path.join(BASE_DIR, 'images')
 # list of all image's file path with ext of jpg
 IMAGES_LIST = glob.glob(os.path.join(IMAGES_PATH, "*.jpg"))
 
-size = (300, 300)
+
 img_index = 0
 
 
@@ -29,23 +29,25 @@ def display_img(index):
 
 
 def on_next():
-	global img_index
+	global img_index, size
 	img_index += 1
 
 	if img_index >= len(IMAGES_LIST):
 		img_index = 0
 
+	size = (root.winfo_width(), root.winfo_height()) 
 	display_img(img_index)
 	update_filename()
 
 
 def on_previous():
-	global img_index
+	global img_index, size
 	img_index -= 1
 
 	if img_index >= len(IMAGES_LIST):
 		img_index = 0
 
+	size = (root.winfo_width(), root.winfo_height()) 
 	display_img(img_index)
 	update_filename()
 
@@ -53,18 +55,21 @@ def on_previous():
 root = tk.Tk()
 root.title("Image Gallery")
 root.geometry("600x500")
+root.update()
 
-mainframe = ttk.Frame(root, padding=10)
-info_frame = ttk.Frame(mainframe)
-image_frame = ttk.Frame(mainframe, relief=tk.SOLID)
-tool_frame = ttk.Frame(mainframe)
+size = (root.winfo_width(), root.winfo_height()) 
+
+mainframe = ttk.Frame(root)
+info_frame = ttk.Frame(mainframe, padding=5)
+image_frame = ttk.Frame(mainframe, padding=5)
+tool_frame = ttk.Frame(mainframe, padding=5)
 mainframe.pack(expand=True, fill=tk.BOTH)
 info_frame.grid(stick=(tk.E, tk.W))
 image_frame.grid(row=1, column=0, sticky=(tk.E, tk.W, tk.S, tk.N))
 tool_frame.grid(row=2, column=0, sticky=(tk.E, tk.W))
 
 img_filename_label = ttk.Label(info_frame, text=IMAGES_LIST[img_index])
-img_filename_label.pack(side=tk.LEFT, padx=3)
+img_filename_label.pack(side=tk.LEFT)
 
 
 # Set first image to label
@@ -77,13 +82,13 @@ label.pack(expand=True)
 
 previous_button = ttk.Button(tool_frame, text="Previous", command=on_previous)
 next_button = ttk.Button(tool_frame, text="Next", command=on_next)
-previous_button.grid(row=0, column=0, sticky=tk.W)
-next_button.grid(row=0, column=1, sticky=tk.E)
+previous_button.grid(row=0, column=0, sticky=tk.E)
+next_button.grid(row=0, column=1, sticky=tk.W)
 tool_frame.columnconfigure(0, weight=1)
 tool_frame.columnconfigure(1, weight=1)
 
-for child in mainframe.winfo_children():
-	child.grid_configure(padx=10, pady=10)
+# for child in mainframe.winfo_children():
+# 	child.grid_configure(padx=10, pady=10)
 
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(1, weight=1)
