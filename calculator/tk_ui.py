569711505ff7d215b24fa881cc3dd5c66fb5af9e
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-25 09:22:46 +0330

    Make the first look of the ui

--- calculator/tk_ui.py
+++ calculator/tk_ui.py
@@ -3,33 +3,55 @@ import tkinter as tk
 from tkinter import ttk
 
 
+def clear_one():
+	pass
+
+def clear_all():
+	pass
+
+def power():
+	pass
+
+def div():
+	pass
+
+
 
 root = tk.Tk()
-root.title('Calculator')
+root.title("Tkinter Calculator")
+mainframe = ttk.Frame(root, padding=10)
+mainframe.pack(expand=True, fill=tk.BOTH)
 
-output_pad = ttk.Frame(root)
-num_pad = ttk.Frame(root)
+output = ttk.Entry(mainframe)
+output.grid(row=0, column=0, columnspan=4)
 
-output = ttk.Entry(output_pad)
-output.grid(row=0, column=0, sticky='wesn')
+col = 0
+for item in ['C', 'CC', '^', '/']:
+	ttk.Button(mainframe, text=item).grid(row=1, column=col)
+	col += 1
 
-# buttons
-col_num = 0
-row_num = 0
-for num in range(10):
-    ttk.Button(num_pad, text=num).grid(row=row_num, column=col_num)
-    if col_num % 3 == 0:
-        col_num = 0
-        row_num += 1
-    col_num += 1
-    
-        
+row = 2
+col = 0
+for num in range(9, 0, -1):
+	if col >= 3:
+		row += 1
+		col = 0
+	ttk.Button(mainframe, text=num).grid(row=row, column=col)
+	col += 1
+
+row = 2
+for item in ['*', '-', '+']:
+	ttk.Button(mainframe, text=item).grid(row=row, column=3)
+	row += 1
 
 
+col = 0
+for item in ['0', '.', '=']:
+	if item == '=':
+		ttk.Button(mainframe, text=item).grid(row=5, column=col, columnspan=2, sticky=(tk.W, tk.E))
+	else:
+		ttk.Button(mainframe, text=item).grid(row=5, column=col)
+	col += 1
 
-
-
-output_pad.grid(row=0, column=0, sticky='nesw')
-num_pad.grid(row=1, column=0)
 
 root.mainloop()
